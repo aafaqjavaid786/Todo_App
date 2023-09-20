3c461219b05585cb6c8243712d8647d7f823deb0
AJ <aafaqjavaid786@gmail.com>
2023-09-20 12:01:05 +0500

    Modifications done

--- todo/urls.py
+++ todo/urls.py
@@ -3,14 +3,14 @@ from django.contrib.auth.views import LoginView, LogoutView
 from . import views
 
 urlpatterns = [
-    path('register', views.UserRegistrationView.as_view(), name='register'),
-    path('login', LoginView.as_view(), name='login'),
-    path('logout', LogoutView.as_view(), name='logout'),
-    path('task/add', views.TaskAddView.as_view(), name='task-add'),
-    path('task/list', views.TaskListView.as_view(), name='task-list'),
-    path('task/all', views.AllTasksView.as_view(),name='all-tasks'),
+    path('register/', views.UserRegistrationView.as_view(), name='register'),
+    path('login/', LoginView.as_view(), name='login'),
+    path('logout/', LogoutView.as_view(), name='logout'),
+    path('task/add/', views.TaskAddView.as_view(), name='task-add'),
+    path('task/list/', views.TaskListView.as_view(), name='task-list'),
+    path('task/all/', views.AllTasksView.as_view(),name='all-tasks'),
     path('task/<str:pk>/', views.TaskDetailView.as_view(), name='task-detail'),
-    path('task/<str:pk>/edit', views.TaskEditView.as_view(), name='task-edit'),
-    path('task/<str:pk>/remove', views.TaskRemoveView.as_view(), name='task-remove'),
-    path('update-task-order', views.UpdateTaskOrderView.as_view(), name='update-task-order'),
+    path('task/<str:pk>/edit/', views.TaskEditView.as_view(), name='task-edit'),
+    path('task/<str:pk>/remove/', views.TaskRemoveView.as_view(), name='task-remove'),
+    path('update-task-order/', views.UpdateTaskOrderView.as_view(), name='update-task-order'),
 ]
